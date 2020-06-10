33691cbe66b88c3af480510decbebd878f0c3c9b
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 21:38:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,12 @@
 class Pokemon
 
-attr_accessor 
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize (id=nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end 
 
 end
