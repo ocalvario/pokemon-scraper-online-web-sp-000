33ddf61f357ea9230b00bdb6ba2ca71d8216c352
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 21:43:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,4 +9,8 @@ class Pokemon
     @db = db
   end 
 
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
 end
